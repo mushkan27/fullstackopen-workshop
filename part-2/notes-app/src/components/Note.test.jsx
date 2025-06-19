1f3dd45e760818343918c8c02f52604431ee24e7
mushkan27 <muskanyon@gmail.com>
2025-06-19 09:29:08 +0545

    test: add user-event for interaction testing in Note component

--- part-2/notes-app/src/components/Note.test.jsx
+++ part-2/notes-app/src/components/Note.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
 
 test('renders content', () => {
@@ -6,9 +7,31 @@ test('renders content', () => {
     content: 'Component testing is done with react-testing-library',
     important: true
   }
-
   render(<Note note={note} />)
 
+  // screen.debug() //prints the full HTML structure of the rendered component to console.
+
   const element = screen.getByText('Component testing is done with react-testing-library')
+
+  screen.debug(element) //only prints the HTML of that specific element to console.
   expect(element).toBeDefined()
+})
+
+test('clicking the button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  const mockHandler = vi.fn()
+
+  render(
+    <Note note={note} updateNote={mockHandler} />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('Change true')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
