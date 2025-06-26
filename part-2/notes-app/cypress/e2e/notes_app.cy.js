38c980d84377df8960ea08bdbb668f4a9210aa31
mushkan27 <muskanyon@gmail.com>
2025-06-26 20:34:29 +0545

    chore: update ESLint config, add Cypress plugin, and improve LoginForm accessibility

--- part-2/notes-app/cypress/e2e/notes_app.cy.js
+++ part-2/notes-app/cypress/e2e/notes_app.cy.js
@@ -1,7 +1,18 @@
 describe('Note app', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:5173')
+  })
+  it('front page can be opened', function() {
     cy.contains('Notes')
     cy.contains('HTML is easy')
+  })
+
+  it('login form can be opened', function() {
+    cy.contains('login').click()
+    cy.get('#username').type('muskan7')
+    cy.get('#password').type('password123')
+    cy.get('#loginButton').click()
+
+    cy.contains('Create a new note')
   })
 })
