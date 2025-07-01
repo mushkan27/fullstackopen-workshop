7f4740c231a7450849d224eaae0353771d14217b
mushkan27 <muskanyon@gmail.com>
2025-07-01 13:44:38 +0545

    feat: update index.html and refactor App component in index.jsx

--- part-6/redux-counter/src/index.jsx
+++ part-6/redux-counter/src/index.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { useState } from 'react'
+// import App from './App.jsx'
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  const addCounter = () => {
+    setCounter(counter+1)
+  }
+
+  const subtractCounter = () => {
+    setCounter(counter - 1)
+  }
+
+  const makeZero = () => {
+    setCounter(0)
+  }
+
+
+  return (
+    <div>
+    <div>{counter}</div>
+    <button onClick={addCounter}>add</button>
+    <button onClick={subtractCounter}>subtract</button>
+    <button onClick={makeZero}>zero</button>
+  </div>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
 )
