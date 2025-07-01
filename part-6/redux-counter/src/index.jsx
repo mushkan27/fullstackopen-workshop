36584df8fd00b411d20a2294d6f1c6369e9aec87
mushkan27 <muskanyon@gmail.com>
2025-07-01 14:18:04 +0545

    feat: refactor counter logic to use Redux for state management

--- part-6/redux-counter/src/index.jsx
+++ part-6/redux-counter/src/index.jsx
@@ -1,27 +1,48 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { useState } from 'react'
-// import App from './App.jsx'
+// import { useState } from 'react'
+import { createStore } from 'redux'
+
+const counterReducer = (state=0, action) => {
+  console.log(action)
+  console.log(state)
+  if(action.type === 'ADD'){
+    const newState = state + 1
+    return newState
+  }else if(action.type === 'SUBTRACT'){
+    return state -1
+  }else if(action.type === 'ZERO'){
+    return 0
+  }
+
+  return state
+}
+
+const store = createStore(counterReducer)
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  // const [counter, setCounter] = useState(0)
 
   const addCounter = () => {
-    setCounter(counter+1)
+    // setCounter(counter+1)
+    store.dispatch({type: 'ADD'})
   }
 
   const subtractCounter = () => {
-    setCounter(counter - 1)
+    // setCounter(counter - 1)
+    store.dispatch({type: 'SUBTRACT'})
   }
 
   const makeZero = () => {
-    setCounter(0)
+    // setCounter(0)
+    store.dispatch({type: 'ZERO'})
   }
 
 
   return (
     <div>
-    <div>{counter}</div>
+    {/* <div>{counter}</div> */}
+    <div>{store.getState()}</div>
     <button onClick={addCounter}>add</button>
     <button onClick={subtractCounter}>subtract</button>
     <button onClick={makeZero}>zero</button>
@@ -29,6 +50,10 @@ const App = () => {
   )
 }
 
-createRoot(document.getElementById('root')).render(
-  <App />
-)
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(<App />)
+store.subscribe(() => {
+  root.render(<App />)
+})
