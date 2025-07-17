8e0ac7d8a9532dfea85ec2a9c1d1657773628402
mushkan27 <muskanyon@gmail.com>
2025-07-17 16:01:45 +0545

    feat: integrate React Query for data fetching and add getNotes service

--- part-6/redux-notes/src/index.jsx
+++ part-6/redux-notes/src/index.jsx
@@ -1,15 +1,19 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import store from './store'
+// import store from './store'
 
-import App from './App'
-import { Provider } from 'react-redux'
+import App from './ReactQueryApp'
+// import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 
 const container = document.getElementById('root')
 const root = createRoot(container)
 
 root.render(
-<Provider store={store}>
-<App />
-</Provider>
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
 )
