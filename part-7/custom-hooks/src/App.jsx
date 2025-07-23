2326f3e6f4103a156b5de7452d51d30857f00b1a
mushkan27 <muskanyon@gmail.com>
2025-07-23 11:03:24 +0545

    feat: -initialize project with ESLint, Prettier, Vite, and React setup -add custom useCounter hook with multiple counter support in App

---
+++ part-7/custom-hooks/src/App.jsx
@@ -0,0 +1,38 @@
+import useCounter from './hooks/useCounter'
+
+const App = () => {
+  const counter1 = useCounter(0)
+  const counter2 = useCounter(0)
+
+  return (
+    <>
+    <div>
+      <div>{counter1.value}</div>
+      <button onClick={counter1.increase}>
+        plus
+      </button>
+      <button onClick={counter1.decrease}>
+        minus
+      </button>      
+      <button onClick={counter1.zero}>
+        zero
+      </button>
+    </div>
+
+    <div>
+      <div>{counter2.value}</div>
+      <button onClick={counter2.increase}>
+        plus
+      </button>
+      <button onClick={counter2.decrease}>
+        minus
+      </button>      
+      <button onClick={counter2.zero}>
+        zero
+      </button>
+    </div>
+    </>
+  )
+}
+
+export default App
