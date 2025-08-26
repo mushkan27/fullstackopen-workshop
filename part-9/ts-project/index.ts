454e6a3a6b84aeed293e0e1396dff87b88d8953d
mushkan27 <muskanyon@gmail.com>
2025-08-26 12:39:45 +0545

    feat: enhance type safety in multiplicator function and update operation handling in index

--- part-9/ts-project/index.ts
+++ part-9/ts-project/index.ts
@@ -1,6 +1,6 @@
 import express from "express"
 const app = express();
-import { multiplicator } from "./multiplier";
+import { multiplicator, Operation } from "./multiplier";
 
 
 app.get('/ping', (_req, res) => {
@@ -9,10 +9,12 @@ app.get('/ping', (_req, res) => {
 
 
 app.post('/calculate', (req, res) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const { value1, value2, op } = req.body;
 
-
-    const result = multiplicator(Number(value1), Number(value2), op);
+    const operation = op as Operation
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const result = multiplicator(Number(value1), Number(value2), operation);
     res.send({result})
 
 })
--- part-9/ts-project/multiplier.ts
+++ part-9/ts-project/multiplier.ts
@@ -1,23 +1,23 @@
-type Operation = 'multiply' | 'add' | 'divide';
-type Result = string | number
+export type Operation = 'multiply' | 'add' | 'divide';
+type Result = string | number;
 
 export const multiplicator = (a: number, b: number, operation: Operation): Result => {
   if(operation === 'divide'){
     if(b===0){
-      throw new Error("Can't divide by 0!")
+      throw new Error("Can't divide by 0!");
     }
-    return a/b
+    return a/b;
   } else if (operation === 'add'){
-    return a + b
+    return a + b;
   }
-  return 'not valid operation'
-  }
+  return 'not valid operation';
+  };
 
   try {
       console.log(multiplicator(2, 0, 'divide'));
   } catch (error: unknown) {
     if(error instanceof Error){
-      console.log('error is: ', error.message)
+      console.log('error is: ', error.message);
     }
   }
   
