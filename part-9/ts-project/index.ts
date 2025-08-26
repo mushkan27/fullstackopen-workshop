3b1b53f25028e96948df32ff02983a21e3c37df8
mushkan27 <muskanyon@gmail.com>
2025-08-26 12:33:12 +0545

    chore: update package.json and tsconfig.json for ESLint integration
    - Added ESLint and related dependencies to package.json.
    - Introduced a new ESLint configuration file (eslint.config.mjs) with TypeScript support.
    - Updated TypeScript version in package.json.
    - Modified tsconfig.json to include an empty types array for better type management.

--- part-9/ts-project/index.ts
+++ part-9/ts-project/index.ts
@@ -1,9 +1,21 @@
 import express from "express"
 const app = express();
+import { multiplicator } from "./multiplier";
+
 
 app.get('/ping', (_req, res) => {
   res.send('pong');
 });
+
+
+app.post('/calculate', (req, res) => {
+    const { value1, value2, op } = req.body;
+
+
+    const result = multiplicator(Number(value1), Number(value2), op);
+    res.send({result})
+
+})
 
 const PORT = 3003;
 
--- part-9/ts-project/multiplier.ts
+++ part-9/ts-project/multiplier.ts
@@ -1,7 +1,7 @@
 type Operation = 'multiply' | 'add' | 'divide';
 type Result = string | number
 
-const multiplicator = (a: number, b: number, operation: Operation): Result => {
+export const multiplicator = (a: number, b: number, operation: Operation): Result => {
   if(operation === 'divide'){
     if(b===0){
       throw new Error("Can't divide by 0!")
