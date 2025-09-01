76f4c45d6b69075a584d9ae922d60796f2f34fb4
mushkan27 <muskanyon@gmail.com>
2025-09-01 14:08:32 +0545

    feat: implement diary retrieval by ID in diary service and update routes

--- part-9/flight_diary/src/routes/diaries.ts
+++ part-9/flight_diary/src/routes/diaries.ts
@@ -7,6 +7,16 @@ router.get('/', (_req, res) => {
   res.send(diaryService.getNonSensitiveDiaryEntries());
 });
 
+router.get('/:id', (req, res) => {
+  const diary = diaryService.findById(Number(req.params.id));
+
+  if (diary) {
+    res.send(diary);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
 router.post('/', (_req, res) => {
   res.send('Saving a diary!');
 });
