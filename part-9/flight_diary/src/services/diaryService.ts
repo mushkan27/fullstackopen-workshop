f119804d919d62b7fa82da62adeeb12324abf0eb
mushkan27 <muskanyon@gmail.com>
2025-09-01 14:26:51 +0545

    feat: implement diary entry addition in diary service and update routes

--- part-9/flight_diary/src/services/diaryService.ts
+++ part-9/flight_diary/src/services/diaryService.ts
@@ -1,5 +1,5 @@
 import diaries from '../../data/entries';
-import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
+import { DiaryEntry, NonSensitiveDiaryEntry, NewDiaryEntry } from '../types';
 
 
 const getEntries = (): DiaryEntry[] => {
@@ -15,13 +15,22 @@ return diaries.map(({id, date, weather, visibility}) => ({
 }));
 };
 
-const addDiary = () => {
-  return null;
-};
-
 const findById = (id: number): DiaryEntry | undefined => {
   const entry = diaries.find(d => d.id === id);
   return entry;
+};
+
+const addDiary = (
+    entry: NewDiaryEntry
+  ): DiaryEntry => {
+
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...entry
+  };
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
 };
 
 export default {
