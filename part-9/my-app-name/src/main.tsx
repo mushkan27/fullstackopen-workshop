61db5068284961e29400c83f2f18b866e1b8d92b
mushkan27 <muskanyon@gmail.com>
2025-09-17 08:35:04 +0545

    fix: remove unnecessary JSX type annotation and update TypeScript configuration

--- part-9/my-app-name/src/main.tsx
+++ part-9/my-app-name/src/main.tsx
@@ -1,11 +1,10 @@
-import { JSX } from 'react';
 import ReactDOM from 'react-dom/client'
 
 interface WelcomeProps {
   name: string;
 }
 
-const Welcome = (props: WelcomeProps): JSX.Element => {
+const Welcome = (props: WelcomeProps) => {
   return <h1>Hello, {props.name}</h1>;
 };
 
